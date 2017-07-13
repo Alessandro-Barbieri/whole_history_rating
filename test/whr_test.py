24e7eb3a934c97f0d84a35e42fae50240acbdf37
Alessandro Barbieri <ale.barbio@alice.it>
2017-07-13 17:46:49 +0200

    test unit translation

---
+++ test/whr_test.py
@@ -0,0 +1,61 @@
+import unittest
+
+class WholeHistoryRatingTest(unittest.TestCase):
+  
+  def setup(self)
+    return(self.whr = WholeHistoryRating.Base.new)
+
+  def setup_game_with_elo(self, white_elo, black_elo, handicap):
+    game = self.whr.create_game("black", "white", "W", 1, handicap)
+    game.black_player.days[0].elo = black_elo
+    game.white_player.days[0].elo = white_elo
+    return game
+
+  def test_even_game_between_equal_strength_players_should_have_white_winrate_of_50_percent(self):
+    game = setup_game_with_elo(500, 500, 0)
+    return assertAlmostEqual(0.5, game.white_win_probability, delta=0.0001)
+
+  def test_handicap_should_confer_advantage(self):
+    game = setup_game_with_elo(500, 500, 1)
+    return assertGreater(game.black_win_probability, 0.5)
+
+  def test_higher_rank_should_confer_advantage(self):
+    game = setup_game_with_elo(600, 500, 0)
+    return assertGreater(game.white_win_probability, 0.5)
+
+  def test_winrates_are_equal_for_same_elo_delta(self):
+    game = setup_game_with_elo(100, 200, 0)
+    game2 = setup_game_with_elo(200, 300, 0)
+    return assertAlmostEqual(game.white_win_probability, game2.white_win_probability, delta=0.0001)
+
+  def test_winrates_for_twice_as_strong_player(self):
+    game = setup_game_with_elo(100, 200, 0)
+    return assertAlmostEqual(0.359935, game.white_win_probability, delta=0.001)
+
+  def test_winrates_should_be_inversely_proportional_with_unequal_ranks(self):
+    game = setup_game_with_elo(600, 500, 0)
+    return assertAlmostEqual(game.white_win_probability, 1 - game.black_win_probability, delta=0.0001)
+  
+  def test_winrates_should_be_inversely_proportional_with_handicap(self):
+    game = setup_game_with_elo(500, 500, 4)
+    return assertAlmostEqual(game.white_win_probability, 1 - game.black_win_probability, delta=0.0001)
+    
+  def test_output(self):
+    self.whr.create_game("shusaku", "shusai", "B", 1, 0)
+    self.whr.create_game("shusaku", "shusai", "W", 2, 0)
+    self.whr.create_game("shusaku", "shusai", "W", 3, 0)
+    self.whr.create_game("shusaku", "shusai", "W", 4, 0)
+    self.whr.create_game("shusaku", "shusai", "W", 4, 0)
+    self.whr.iterate(50)
+    assertListEqual([[1, -92, 71], [2, -94, 71], [3, -95, 71], [4, -96, 72]], self.whr.ratings_for_player("shusaku"))
+    assertListEqual([[1, 92, 71], [2, 94, 71], [3, 95, 71], [4, 96, 72]], self.whr.ratings_for_player("shusai"))
+  
+  def test_unstable_exception_raised_in_certain_cases(self):
+    for(game in range(1, 10)):
+       self.whr.create_game("anchor", "player", "B", 1, 0)
+       self.whr.create_game("anchor", "player", "W", 1, 0)
+    for(game in range(1,10)):
+       self.whr.create_game("anchor", "player", "B",180, 600)
+       self.whr.create_game("anchor", "player", "W",180, 600)
+    with assertRaises(WholeHistoryRating.UnstableRatingException):
+      self.whr.iterate(10)
